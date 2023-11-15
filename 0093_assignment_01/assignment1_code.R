bd9ad32902e30bc572de15274a93d610afa60686
Jichuan <91568110+jichuan-zhang@users.noreply.github.com>
2023-11-15 10:34:37 +0000

    split code to data processing and plotting, did some plot. Some comment and a map to be added later.

--- 0093_assignment_01/assignment1_code.R
+++ 0093_assignment_01/assignment1_code.R
@@ -1,6 +1,7 @@
 library(sf)
 library(rstudioapi)
 library(dplyr)
+library(tidyr)
 library(tmap)
 library(viridisLite)
 
@@ -19,74 +20,27 @@ charging_ports_sf = st_as_sf(charging_ports, coords = c("longitude", "latitude")
 # Filter out charging port NOT in wales/england
 charging_ports_in_lsoa = st_filter(charging_ports_sf, lsoa_boundaries)
 
-
 # Join income data with LSOA geometry
 # Although doesn't matter, inner makes most sense here because missing on either side should be omitted
-income_join = inner_join(lsoa_boundaries, income, join_by("LSOA11CD" == "LSOA.code"))
+lsoa_all = inner_join(lsoa_boundaries, income, join_by("LSOA11CD" == "LSOA.code"))
 
-# Check for any invalid geometry before plotting it (takes a long time to plot!)
-validity = st_is_valid(income_join)
-invalid_geometries = income_join[!validity, ]
-
-
-# Actual Plotting
-tmap_mode("plot")
-
-# Change bounding box
-# Get current bounding box
-bbox_new = st_bbox(income_join) 
-
-# Get range of x and y
-xrange = bbox_new$xmax - bbox_new$xmin
-yrange = bbox_new$ymax - bbox_new$ymin
-
-# Modify each direction
-# bbox_new[1] = bbox_new[1] - (0.25 * xrange) # xmin - left
-# bbox_new[3] = bbox_new[3] + (0.25 * xrange) # xmax - right
-# bbox_new[2] = bbox_new[2] - (0.25 * yrange) # ymin - bottom
-bbox_new[4] = bbox_new[4] + (0.2 * yrange) # ymax - top
-
-# Change bbox to sf polygon
-bbox_new = st_as_sfc(bbox_new)
-
-# 
-income_layer =
-  tm_shape(income_join, bbox = bbox_new)+
-  tm_fill(col = "Income", alpha = 0.7, palette = "inferno",
-          style = "quantile", legend.show = FALSE)
-
-charging_ports_layer =
-  tm_shape(charging_ports_in_lsoa) +
-  tm_dots(col = "blue", size = 0.00002)
-
-legends = 
-  tm_add_legend(type = "symbol", col = c(inferno(5), "blue"),
-                border.col = "black", border.lwd = 0.2,size = 1,
-                shape = c(22, 22, 22, 22, 22, 21),
-                labels = c("2,241 to 15,259",
-                           "15,259 to 16,438",
-                           "16,438 to 17,584",
-                           "17,584 to 19,319",
-                           "19,319 to 51,116",
-                           "EV Charger"),
-                title = "Individual Income")
-
-datasource = 
-  tm_credits("Source: \nOffice for National Statistics \nNational Chargepoint Registry",
-             size = 0.5, position = c("left", "bottom"))
-
-map =
-  income_layer+
-  charging_ports_layer+
-  legends+
-  datasource+
-  tm_layout(legend.show = TRUE, 
-            legend.position = c(0.05, 0.65),
-            legend.title.fontface = "bold",
-            title = "EV Charging Port VS Income",
-            title.position = c("center", "top")) +
-  tm_logo("Energy Institute Logo.png", height = 2, position = c("left", "top")) +
-  tm_scale_bar(position = c("right", "bottom"), width = 0.25) +
-  tm_compass(position = c("right", "top"), size = 2.5)
-
-tmap_save(map, filename = "EV Charging Port VS Income v1.5.png", width = 1440, height = 1920)
+# Count how many charging port per LSOA
+# Assign charging ports to LSOA
+charging_ports_in_lsoa = st_join(charging_ports_in_lsoa, lsoa_all, join = st_within)
+# Count them
+charging_ports_count = summarize(
+  group_by(charging_ports_in_lsoa, LSOA11CD),
+  count = n())
+# Convert the sf to dataframe for left join, also removing geometry column to avoid conflict
+charging_ports_count_df = as.data.frame(charging_ports_count)
+charging_ports_count_df = charging_ports_count_df[, -3]
+# Add the charging port count data to the sf
+lsoa_all = left_join(lsoa_all, charging_ports_count_df, by = c("LSOA11CD" = "LSOA11CD"))
+# Calculate income per charging port, intensionally not removing NA
+lsoa_all = mutate(lsoa_all, income_per_charging_port = Income / count)
+lsoa_all = mutate(lsoa_all, area_per_charging_port = Shape__Area / count)
+# Save as geojson
+lsoa_all = lsoa_all %>%
+  select(-LSOA11NMW, -BNG_E, -BNG_N, -LONG, -LAT, -GlobalID, -LSOA.name, -Shape__Length)
+st_write(lsoa_all, "assignment_1_lsoa.geojson")
+st_write(charging_ports_in_lsoa, "charging_ports_in_lsoa.geojson")
---
+++ 0093_assignment_01/assignment1_plotting.R
@@ -0,0 +1,134 @@
+library(sf)
+library(rstudioapi)
+library(tmap)
+library(viridisLite)
+
+setwd(dirname(rstudioapi::getSourceEditorContext()$path))
+
+charging_ports_in_lsoa = st_read("charging_ports_in_lsoa.geojson")
+lsoa_all = st_read("assignment_1_lsoa.geojson")
+
+tmap_mode("plot")
+
+# Change bounding box
+# Get current bounding box
+bbox_new = st_bbox(lsoa_all) 
+
+# Get range of x and y
+xrange = bbox_new$xmax - bbox_new$xmin
+yrange = bbox_new$ymax - bbox_new$ymin
+
+# Modify each direction
+# bbox_new[1] = bbox_new[1] - (0.25 * xrange) # xmin - left
+# bbox_new[3] = bbox_new[3] + (0.25 * xrange) # xmax - right
+# bbox_new[2] = bbox_new[2] - (0.25 * yrange) # ymin - bottom
+bbox_new[4] = bbox_new[4] + (0.2 * yrange) # ymax - top
+
+# Change bbox to sf polygon
+bbox_new = st_as_sfc(bbox_new)
+
+
+income =
+  tm_shape(lsoa_all, bbox = bbox_new)+
+  tm_fill(col = "Income", alpha = 0.7, palette = "inferno",
+          style = "quantile", legend.show = FALSE)
+
+charging_ports_count =
+  tm_shape(lsoa_all, bbox = bbox_new)+
+  tm_fill(col = "count", alpha = 0.7, palette = "inferno",
+          breaks = c(1, 2, 3, 4, 7, Inf), 
+          legend.show = FALSE)
+
+income_per_charging_port =
+  tm_shape(lsoa_all, bbox = bbox_new)+
+  tm_fill(col = "income_per_charging_port", alpha = 0.7, palette = "-inferno",
+          style = "quantile", legend.show = FALSE)
+
+charging_ports_scatter =
+  tm_shape(charging_ports_in_lsoa) +
+  tm_dots(col = "blue", size = 0.00002)
+
+legends1 =
+  tm_add_legend(type = "symbol", col = c(inferno(5), "blue"),
+                border.col = "black", border.lwd = 0.2,size = 1,
+                shape = c(22, 22, 22, 22, 22, 21),
+                labels = c("2,241 to 15,259",
+                           "15,259 to 16,438",
+                           "16,438 to 17,584",
+                           "17,584 to 19,319",
+                           "19,319 to 51,116",
+                           "EV Charger"),
+                title = "Individual Income")
+
+
+legends2 = 
+  tm_add_legend(type = "symbol", col = c(inferno(5), "grey"),
+                border.col = "black", border.lwd = 0.2,size = 1,
+                shape = c(22, 22, 22, 22, 22, 22),
+                labels = c("1",
+                           "2",
+                           "3",
+                           "4 to 6",
+                           "7 or more",
+                           "No Charger in LSOA"),
+                title = "Income per EV Charger")
+
+legends3 = 
+  tm_add_legend(type = "symbol", col = c(rev(inferno(5)), "grey"),
+                border.col = "black", border.lwd = 0.2,size = 1,
+                shape = c(22, 22, 22, 22, 22, 22),
+                labels = c("119 to 3,325",
+                           "3,325 to 5,827",
+                           "5,827 to 8,874",
+                           "8.874 to 16.283",
+                           "16.283 to 50,102",
+                           "No Charger in LSOA"),
+                title = "Income per EV Charger")
+
+datasource =
+  tm_credits("Source: \nOffice for National Statistics \nNational Chargepoint Registry",
+             size = 0.5, position = c("left", "bottom"))
+
+other_stuff =
+  tm_logo("Energy Institute Logo.png", height = 2, position = c("left", "top")) +
+  tm_scale_bar(position = c("right", "bottom"), width = 0.25) +
+  tm_compass(position = c("right", "top"), size = 2.5)
+
+# First Map - Scattered Charging Port on Income
+map1 =
+  income+
+  charging_ports_scatter+
+  legends1+
+  datasource+
+  tm_layout(legend.show = TRUE, 
+            legend.position = c(0.05, 0.65),
+            legend.title.fontface = "bold",
+            title = "Scattered Charging Port",
+            title.position = c("center", "top"))+
+  other_stuff
+
+# Second Map - Income per EV Charger
+map2 =
+  charging_ports_count+
+  legends2+   
+  datasource+
+  tm_layout(legend.show = TRUE,
+            legend.position = c(0.05, 0.65),
+            legend.title.fontface = "bold",
+            title = "Number of EV Charging Port",
+            title.position = c("center", "top"))+
+  other_stuff
+
+# Third Map - Income per EV Charger
+map3 =
+  income_per_charging_port+
+  legends3+   
+  datasource+
+  tm_layout(legend.show = TRUE,
+            legend.position = c(0.05, 0.65),
+            legend.title.fontface = "bold",
+            title = "Income per EV Charging Port",
+            title.position = c("center", "top"))+
+  other_stuff
+
+tmap_save(map1, filename = "Scattered Charging Port on Income 17.png", width = 1440, height = 1920)
