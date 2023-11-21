98b0ec83cb15aed2f925ab9532ab20826adbf558
Jichuan <91568110+jichuan-zhang@users.noreply.github.com>
2023-11-21 15:29:11 +0000

    more plotting for assigment

--- 0093_assignment_01/assignment1_plotting.R
+++ 0093_assignment_01/assignment1_plotting.R
@@ -13,28 +13,28 @@ tmap_mode("plot")
 # Change bounding box
 # Get current bounding box
 bbox_new = st_bbox(lsoa_all) 
+bbox_aux = st_bbox(lsoa_all)
 
 # Get range of x and y
 xrange = bbox_new$xmax - bbox_new$xmin
 yrange = bbox_new$ymax - bbox_new$ymin
 
 # Modify each direction
-# bbox_new[1] = bbox_new[1] - (0.25 * xrange) # xmin - left
-# bbox_new[3] = bbox_new[3] + (0.25 * xrange) # xmax - right
+bbox_new[1] = bbox_new[1] - (0.25 * xrange) # xmin - left
+bbox_new[3] = bbox_new[3] + (0.25 * xrange) # xmax - right
 # bbox_new[2] = bbox_new[2] - (0.25 * yrange) # ymin - bottom
 bbox_new[4] = bbox_new[4] + (0.2 * yrange) # ymax - top
 
 # Change bbox to sf polygon
 bbox_new = st_as_sfc(bbox_new)
 
-
 income =
-  tm_shape(lsoa_all, bbox = bbox_new)+
+  tm_shape(lsoa_all, bbox = bbox_aux)+
   tm_fill(col = "Income", alpha = 0.7, palette = "inferno",
           style = "quantile", legend.show = FALSE)
 
 charging_ports_count =
-  tm_shape(lsoa_all, bbox = bbox_new)+
+  tm_shape(lsoa_all, bbox = bbox_aux)+
   tm_fill(col = "count", alpha = 0.7, palette = "inferno",
           breaks = c(1, 2, 3, 4, 7, Inf), 
           legend.show = FALSE)
@@ -45,7 +45,7 @@ income_per_charging_port =
           style = "quantile", legend.show = FALSE)
 
 area_per_charging_port = 
-  tm_shape(lsoa_all, bbox = bbox_new)+
+  tm_shape(lsoa_all, bbox = bbox_aux)+
   tm_fill(col = "count", convert2density = TRUE, area = "Shape__Area",
           alpha = 0.7, palette = "inferno",
           style = "quantile", legend.show = FALSE)
@@ -77,7 +77,7 @@ legends2 =
                            "4 to 6",
                            "7 or more",
                            "No Charger in LSOA"),
-                title = "Number of EV Charger")
+                title = "Number of EV Charger\nwithin LSOA")
 
 legends3 = 
   tm_add_legend(type = "symbol", col = c(rev(inferno(5)), "grey"),
@@ -103,12 +103,7 @@ legends4 =
                            "No Charger in LSOA"),
                 title = "Area Density / m2")
 
-datasource =
-  tm_credits("Source: \nOffice for National Statistics \nNational Chargepoint Registry",
-             size = 0.5, position = c("left", "bottom"))
-
 other_stuff =
-  tm_logo("Energy Institute Logo.png", height = 2, position = c("left", "top")) +
   tm_scale_bar(position = c("right", "bottom"), width = 0.25) +
   tm_compass(position = c("right", "top"), size = 2.5)
 
@@ -117,48 +112,42 @@ map1 =
   income+
   charging_ports_scatter+
   legends1+
-  datasource+
   tm_layout(legend.show = TRUE, 
-            legend.position = c(0.05, 0.65),
-            legend.title.fontface = "bold",
-            title = "Scattered Charging Port",
-            title.position = c("center", "top"))+
+            legend.position = c("left", "top"),
+            legend.title.fontface = "bold")+
   other_stuff
 
-# Second Map - Income per EV Charger
+# Second Map - Number of EV Charger
 map2 =
   charging_ports_count+
-  legends2+   
-  datasource+
+  legends2+
   tm_layout(legend.show = TRUE,
-            legend.position = c(0.05, 0.65),
-            legend.title.fontface = "bold",
-            title = "Number of EV Charging Port",
-            title.position = c("center", "top"))+
+            legend.position = c("left", "top"),
+            legend.title.fontface = "bold")+
   other_stuff
 
 # Third Map - Income per EV Charger
 map3 =
   income_per_charging_port+
-  legends3+   
-  datasource+
+  legends3+
+  tm_credits("Source: \nOffice for National Statistics \nNational Chargepoint Registry",
+             size = 0.5, position = c("left", "bottom"))+
   tm_layout(legend.show = TRUE,
-            legend.position = c(0.05, 0.65),
+            legend.position = c("left",0.58),
             legend.title.fontface = "bold",
-            title = "Income per EV Charging Port",
-            title.position = c("center", "top"))+
+            title = "Income per EV Charger",
+            title.position = c("center", "top"),
+            title.size = 1.6)+
+  tm_logo("Energy Institute Logo.png", height = 4, position = c("left", "top"))+
   other_stuff
 
-# Fourth Map - Income per EV Charger
+# Fourth Map - Area Density
 map4 =
   area_per_charging_port+
-  legends4+   
-  datasource+
+  legends4+
   tm_layout(legend.show = TRUE,
-            legend.position = c(0.05, 0.65),
-            legend.title.fontface = "bold",
-            title = "EV Charging Port Area Density",
-            title.position = c("center", "top"))+
+            legend.position = c("left", "top"),
+            legend.title.fontface = "bold")+
   other_stuff
 
-tmap_save(map2, filename = "Number of EV Charging Port 2.png", width = 1440, height = 1920)
+tmap_save(map3, filename = "Income per EV Charging Port 14.png", width = 1920, height = 1920)
