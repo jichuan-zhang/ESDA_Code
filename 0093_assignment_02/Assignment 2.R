c53b6f40100d83ea8650d8045ddd4c6ce5fef9c3
Jichuan <91568110+jichuan-zhang@users.noreply.github.com>
2024-01-14 08:52:15 +0000

    111

--- 0093_assignment_02/Assignment 2.R
+++ 0093_assignment_02/Assignment 2.R
@@ -1,13 +1,14 @@
-library(tmap)
+# Load necessary libraries
+library(raster)
 library(sf)
-library(readxl)
-library(rstudioapi)
-library(dplyr)
+library(gstat)
+library(sp)
 
-# Setting working directory
+# Your existing code to set the working directory and load the data
 setwd(dirname(rstudioapi::getSourceEditorContext()$path))
+indonesia_shape <- st_read("idn_adm_bps_20200401_shp/idn_admbnda_adm0_bps_20200401.shp")
+solar_radiance_brick <- brick("Solar Radiance.nc")
+solar_radiance_avg <- calc(solar_radiance_brick, mean, na.rm = TRUE)
 
-# Load the Shapefile
-indonesia_country <- st_read("idn_adm_bps_20200401_shp/idn_admbnda_adm0_bps_20200401.shp")
 
 
